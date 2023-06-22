2f14fd2ae2c563f6ef760cea9cb4b7c65e8ed25b
DennisBa2604 <21bade1bif@hft-stuttgart.de>
2023-06-22 23:50:36 +0200

    v1.0.4 fixed make Corners

--- main.cpp
+++ main.cpp
@@ -6,10 +6,10 @@
 #include <random>
 
 int size = 0;
-int minSize=0;
+int minSize = 0;
 
 int main() {
-    int** sudoku = makeSudoku(9);
+    int **sudoku = makeSudoku(9);
     sudokuOut(sudoku);
 
     // Deallocate memory for the 2D array
@@ -23,24 +23,26 @@ int main() {
 /*
  * Method to generate a Sudoku grid with a random but possible solution
  */
-int** makeSudoku(int pSize){
-    size=pSize;
-    minSize= sqrt(size);
-    int** sudoku = new int*[size];
-    for(int i=0;i < size;i++){
-        sudoku[i]=new int[size];
+int **makeSudoku(int pSize) {
+    size = pSize;
+    minSize = sqrt(size);
+    int **sudoku = new int *[size];
+    for (int i = 0; i < size; i++) {
+        sudoku[i] = new int[size];
     }
 
-    for(int j = 0; j < size; j++){
-        for(int k = 0; k < size; k++){
+    for (int j = 0; j < size; j++) {
+        for (int k = 0; k < size; k++) {
             sudoku[j][k] = 0;
         }
     }
 
+    sudoku = makeCorners(sudoku);
 
-    sudoku=makeDiagonal(sudoku);
+    //sudoku=makeDiagonal(sudoku);
+    /*
 
-    //makeBorder(sudoku);
+    makeBorder(sudoku);
     std::vector<int> borderOptions = getBorderOptions(0, 3, sudoku);
     for(int i = 0;i<borderOptions.size();i++){
         std::cout<<borderOptions.at(i)<<std::endl;
@@ -49,7 +51,7 @@ int** makeSudoku(int pSize){
 
     std::cout<<std::endl;
 
-
+*/
     return sudoku;
 }
 
@@ -57,17 +59,17 @@ int** makeSudoku(int pSize){
 /*
  * Method to fill a main diagonal with random integers without a duplicate within a 3x3 box
  */
-int** makeDiagonal(int** sudoku){
-    for(int c = 0;c<minSize;c++){
+int **makeDiagonal(int **sudoku) {
+    for (int c = 0; c < minSize; c++) {
         std::vector<int> optionVector;
-        for(int i = 1; i<=size;i++){
+        for (int i = 1; i <= size; i++) {
             optionVector.push_back(i);
         }
-        for(int i = 0+(c*minSize);i<minSize+(c*minSize);i++){
-            for( int j = 0+(c*minSize);j<minSize+(c*minSize);j++){
-                int temp= getRandomInt(optionVector.size());
-                sudoku[i][j]=optionVector.at(temp);
-                optionVector.erase(optionVector.begin()+temp);
+        for (int i = 0 + (c * minSize); i < minSize + (c * minSize); i++) {
+            for (int j = 0 + (c * minSize); j < minSize + (c * minSize); j++) {
+                int temp = getRandomInt(optionVector.size());
+                sudoku[i][j] = optionVector.at(temp);
+                optionVector.erase(optionVector.begin() + temp);
             }
         }
     }
@@ -80,24 +82,25 @@ int** makeDiagonal(int** sudoku){
  *
  * YET TO IMPLEMENT
  */
-int** makeBorder(int** sudoku){
-    int*** borderArray=new int**[size];
-    for(int i = 0;i<size;i++){
-        borderArray[i]=new int*[size];
+int **makeBorder(int **sudoku) {
+    int ***borderArray = new int **[size];
+    for (int i = 0; i < size; i++) {
+        borderArray[i] = new int *[size];
     }
-    for(int i = 0; i<size;i++){
-       for(int j = 0;j<size;j++){
-           borderArray[i][j]=new int[size - minSize];
-       }
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            borderArray[i][j] = new int[size - minSize];
+        }
     }
 
     std::vector<int> borderOptions;
-    int bCounter=0;
-    for(int i = 0; i<size;i++){
-        for(int j=0;j<size;j++){
-            if(i/minSize==j/minSize)continue;
-            if(i/minSize==minSize-1&&j/minSize==0
-                || j/minSize==minSize-1&&i/minSize==0)continue;
+    int bCounter = 0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (i / minSize == j / minSize)continue;
+            if (i / minSize == minSize - 1 && j / minSize == 0
+                || j / minSize == minSize - 1 && i / minSize == 0)
+                continue;
 
             /*
             borderOptions=getBorderOptions(i,j,sudoku);
@@ -106,12 +109,12 @@ int** makeBorder(int** sudoku){
             }
             std::cout << std::endl;*/
 
-            if(sizeof(borderOptions)/ sizeof(int)!=0){
-                int ran=rand()%(sizeof(borderOptions)/sizeof(int));
-                sudoku[i][j]=borderOptions[ran];
-                borderOptions[ran]=0;
-            }else{
-                borderArray[i][j][0]= sudoku[i][j];
+            if (sizeof(borderOptions) / sizeof(int) != 0) {
+                int ran = rand() % (sizeof(borderOptions) / sizeof(int));
+                sudoku[i][j] = borderOptions[ran];
+                borderOptions[ran] = 0;
+            } else {
+                borderArray[i][j][0] = sudoku[i][j];
 
             }
 
@@ -123,36 +126,36 @@ int** makeBorder(int** sudoku){
 /*
  * Method to get possible integers for a slot in a border-box
  */
-std::vector<int> getBorderOptions(int row,int col,int** sudoku){
+std::vector<int> getBorderOptions(int row, int col, int **sudoku) {
     std::vector<int> restrictions;
     //adds the already used numbers from the row to the restrictions array
-    for(int i = 0; i<size;i++){
-        if(!contains(restrictions,sudoku[row][i])&&sudoku[row][i]!=0){
-            restrictions.insert(restrictions.end(),sudoku[row][i]);
+    for (int i = 0; i < size; i++) {
+        if (!contains(restrictions, sudoku[row][i]) && sudoku[row][i] != 0) {
+            restrictions.insert(restrictions.end(), sudoku[row][i]);
         }
     }
     //adds the already used numbers from the col to the restrictions array
-    for(int i = 0; i<size;i++){
-        if(!contains(restrictions,sudoku[i][col])&&sudoku[i][col]!=0){
-            restrictions.insert(restrictions.cend(),sudoku[i][col]);
+    for (int i = 0; i < size; i++) {
+        if (!contains(restrictions, sudoku[i][col]) && sudoku[i][col] != 0) {
+            restrictions.insert(restrictions.cend(), sudoku[i][col]);
         }
     }
     //adds the already used numbers of the same box to the restictions array
-    int blockRow= row / minSize;
-    int blockCol= col / minSize;
+    int blockRow = row / minSize;
+    int blockCol = col / minSize;
 
-    for(int i = 0+ minSize * blockRow; i < minSize+minSize * blockRow; i++){
-        for(int j = 0 + minSize * blockCol; j < minSize+minSize * blockCol; j++){
-            if(!contains(restrictions,sudoku[i][j])&&sudoku[i][j]!=0){
-                restrictions.insert(restrictions.end(),sudoku[i][j]);
+    for (int i = 0 + minSize * blockRow; i < minSize + minSize * blockRow; i++) {
+        for (int j = 0 + minSize * blockCol; j < minSize + minSize * blockCol; j++) {
+            if (!contains(restrictions, sudoku[i][j]) && sudoku[i][j] != 0) {
+                restrictions.insert(restrictions.end(), sudoku[i][j]);
             }
         }
     }
 
     std::vector<int> allowed;
-    for(int i =1;i<=size;i++){
-        if(!contains(restrictions,i)){
-            allowed.insert(allowed.end(),i);
+    for (int i = 1; i <= size; i++) {
+        if (!contains(restrictions, i)) {
+            allowed.insert(allowed.end(), i);
         }
     }
     return allowed;
@@ -161,9 +164,9 @@ std::vector<int> getBorderOptions(int row,int col,int** sudoku){
 /*
  * Method to get a boolean depending on a comparison between array value and test-integer
  */
-bool contains(std::vector<int> array,int test){
-    for(int i=0;i<array.size();i++){
-        if(array.at(i)==test)return true;
+bool contains(std::vector<int> array, int test) {
+    for (int i = 0; i < array.size(); i++) {
+        if (array.at(i) == test)return true;
     }
     return false;
 }
@@ -174,10 +177,10 @@ bool contains(std::vector<int> array,int test){
  * Ansatz fue True Random anhand von Hardware
  * (davor Problem immer selbes Sudoku)
  */
-int getRandomInt(int vectorSize){
+int getRandomInt(int vectorSize) {
     std::random_device rd;// Obtain a random seed from the hardware
     std::default_random_engine engine(rd());
-    std::uniform_int_distribution<int> distribution(0, vectorSize-1);
+    std::uniform_int_distribution<int> distribution(0, vectorSize - 1);
     int randomInt = distribution(engine);
 
     return randomInt;
@@ -186,89 +189,95 @@ int getRandomInt(int vectorSize){
 /*
  * function to fill the missing corners
  */
-int** makeCorners(int** sudoku){
-    for(int num = 1;num<=size;num++){
-        int x,y;
-        for(int i = 0;i<minSize;i++){
-            bool contains=false;
-            for(int j = 0;j<size-minSize;j++){
-                if(sudoku[i][j]==num){
-                    contains=true;
+int **makeCorners(int **sudoku) {
+    for (int num = 1; num <= size; num++) {
+        int x, y;
+        for (int i = 0; i < minSize; i++) {
+            bool contains = false;
+            for (int j = 0; j < size - minSize; j++) {
+                if (sudoku[i][j] == num) {
+                    contains = true;
                     break;
                 }
             }
-            if(contains==false){
-                x=i;
+            if (contains == false) {
+                x = i;
                 break;
             }
         }
-        for(int i = 0;i<3;i++){
-            bool contains=false;
-            for(int j = 0;j<size-minSize;j++){
-                if(sudoku[j][i]==num){
-                    contains=true;
+        for (int i = size - minSize; i < size; i++) {
+            bool contains = false;
+            for (int j = minSize; j < size; j++) {
+                if (sudoku[j][i] == num) {
+                    contains = true;
                     break;
                 }
             }
-            if(contains==false){
-                y=i;
+            if (contains == false) {
+                y = i;
                 break;
             }
         }
-        sudoku[x][y]=num;
+        sudoku[x][y] = num;
     }
-    for(int num = 1;num<=size;num++){
-        int x,y;
-        for(int i = size-minSize;i<size;i++){
-            bool contains=false;
-            for(int j = 0;j<size-minSize;j++){
-                if(sudoku[j][i]==num){
-                    contains=true;
+
+    for (int num = 1; num <= size; num++) {
+        std::cout << num << std::endl;
+        int x, y;
+        for (int i = 0; i < minSize; i++) {
+            bool contains = false;
+            for (int j = 0; j < size - minSize; j++) {
+                if (sudoku[j][i] == num) {
+                    contains = true;
                     break;
                 }
             }
-            if(contains==false){
-                x=i;
+            if (contains == false) {
+                std::cout << "x=" << i << std::endl;
+                x = i;
                 break;
             }
         }
-        for(int i = 0;i<3;i++){
-            bool contains=false;
-            for(int j = 0;j<size-minSize;j++){
-                if(sudoku[j][i]==num){
-                    contains=true;
+        for (int i = size - minSize; i < size; i++) {
+            bool contains = false;
+            for (int j = minSize; j < size; j++) {
+                if (sudoku[i][j] == num) {
+                    contains = true;
                     break;
                 }
             }
-            if(contains==false){
-                y=i;
+            if (contains == false) {
+                std::cout << "y=" << i << std::endl;
+                y = i;
                 break;
             }
         }
-        sudoku[x][y]=num;
+
+        sudoku[y][x] = num;
     }
+    return sudoku;
 }
 
 /*
  * Method to print Sudoku grid onto the console
  */
-void sudokuOut(int** sudoku){
-    for(int i=0;i<size;i++){
-        if ( i % (int) sqrt(size) == 0&&i!=0){
+void sudokuOut(int **sudoku) {
+    for (int i = 0; i < size; i++) {
+        if (i % (int) sqrt(size) == 0 && i != 0) {
 
-            std::cout  << "----------+-----------+---------" << std::endl;
+            std::cout << "----------+-----------+---------" << std::endl;
         }
-        for(int j =0; j<size;j++){
-            if ( j % (int) sqrt(size) == 0 && j != 0){
+        for (int j = 0; j < size; j++) {
+            if (j % (int) sqrt(size) == 0 && j != 0) {
 
-                std::cout  << " | ";
+                std::cout << " | ";
             }
-            std::cout << " "<<sudoku[i][j]<<" ";
+            std::cout << " " << sudoku[i][j] << " ";
 
 
         }
 
-        std::cout<<std::endl;
+        std::cout << std::endl;
 
     }
 }
